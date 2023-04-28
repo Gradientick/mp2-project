d87f9a18a1b5bcecbc9874653744a46b907a36d5
gradientick <camus.luis95@gmail.com>
2023-04-28 17:50:58 +0800

    added peso symbol

--- main.js
+++ main.js
@@ -101,6 +101,15 @@
   },
 ]; */
 
+const categoryButtons = document.querySelectorAll("[data-category-button]");
+categoryButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const buttonClicked = event.target.textContent;
+  });
+});
+
+const filterResults = (buttonClicked) => {};
+
 const fetchNewProduct1 = async () => {
   const response1 = fetch("https://fakestoreapi.com/products");
   const data1 = (await response1).json();
@@ -121,12 +130,17 @@ const generateProducts1 = async () => {
     title.textContent = product.title;
 
     const price = document.createElement("p");
-    price.textContent = product.price;
+    price.textContent = `₱ ${product.price}`;
+
+    const addToCartBtn = document.createElement("button");
+    addToCartBtn.classList.add("add-cart-button");
+    addToCartBtn.textContent = "Add to Cart";
 
     document.getElementById("product-list").appendChild(card);
     card.appendChild(image);
     card.appendChild(title);
     card.appendChild(price);
+    card.appendChild(addToCartBtn);
   });
 };
 
