99a501a108aeb52a9bf16d49b41d84e448c780f1
gradientick <camus.luis95@gmail.com>
2023-05-09 17:31:32 +0800

    filter feature update

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 let category = "";
-
-const categoryButtons = document.querySelectorAll("[data-category-button]");
-categoryButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const buttonClicked = event.target.textContent;
-    category = "";
-    category += buttonClicked;
-  });
-});
-
+// const categoryButtons = document.querySelectorAll("[data-category-button]");
+// categoryButtons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     const buttonClicked = event.target.textContent;
+//     category = "";
+//     category += buttonClicked;
+//     switch (category) {
+//       case "women's clothing":
+//         console.log(category);
+//         break;
+//       case "men's clothing":
+//         console.log(category);
+//         break;
+//       case "jewelery":
+//         console.log(category);
+//         break;
+//       case "electronics":
+//         console.log(category);
+//         break;
+//     }
+//   });
+// });
+const filterAndUpdateItems = () => {
+  filterItemsByCategory(product1, category);
+  updateItemsList(filteredItems);
+};
 const filterItemsByCategory = (items, category) => {
   const filteredItems = items.filter((item) => item.category === category);
   return filteredItems;
@@ -48,7 +64,11 @@ const updateItemsList = (product) => {
     card.appendChild(addToCartBtn);
   });
 };
-
+const removeItems = (product) => {
+  product.forEach(function (product) {
+    const toRemoveItems = document.getElementsByClassName("item");
+  });
+};
 const updateItemsList2 = (product) => {
   product.forEach(function (product2) {
     const card = document.createElement("div");
@@ -80,6 +100,39 @@ const generateProducts1 = async () => {
   const data1 = await fetchNewProduct1();
   const product1 = data1;
   updateItemsList(product1);
+  const categoryButtons = document.querySelectorAll("[data-category-button]");
+  categoryButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const buttonClicked = event.target.textContent;
+      category = "";
+      category += buttonClicked;
+      switch (category) {
+        case "women's clothing":
+          let itemCards = document.querySelectorAll("item");
+          console.log(itemCards);
+          var filteredItems = filterItemsByCategory(product1, category);
+          updateItemsList(filteredItems);
+          break;
+        case "men's clothing":
+          var filteredItems = filterItemsByCategory(product1, category);
+          updateItemsList(filteredItems);
+          break;
+        case "jewelery":
+          var filteredItems = filterItemsByCategory(product1, category);
+          updateItemsList(filteredItems);
+          break;
+        case "electronics":
+          var filteredItems = filterItemsByCategory(product1, category);
+          updateItemsList(filteredItems);
+          break;
+        default:
+          return;
+      }
+    });
+  });
+
+  // let filteredItems = filterItemsByCategory(product1, category);
+  // updateItemsList(filteredItems);
   // category = "women's clothing"
   // let filteredItems = filterItemsByCategory(product1, category);
   // product1.forEach(function (product) {
@@ -122,18 +175,18 @@ generateProducts1();
 
 //Second Set of Product API
 
-const fetchNewProduct2 = async () => {
-  const response2 = fetch("https://dummyjson.com/products");
-  const data2 = (await response2).json();
-  return data2;
-};
+// const fetchNewProduct2 = async () => {
+//   const response2 = fetch("https://dummyjson.com/products");
+//   const data2 = (await response2).json();
+//   return data2;
+// };
 
-const generateProducts2 = async () => {
-  const data2 = await fetchNewProduct2();
-  const product2 = data2.products;
-  updateItemsList2(product2);
-};
-generateProducts2();
+// const generateProducts2 = async () => {
+//   const data2 = await fetchNewProduct2();
+//   const product2 = data2.products;
+//   updateItemsList2(product2);
+// };
+// generateProducts2();
 
 //Modal
 const openModal = document.getElementById("openModal");
